fc431b66f147cdd9246638a8c584ac8bdf87ea1c
axelzucho <axelzucho1@gmail.com>
2017-10-15 21:16:59 -0500

    Corrected functions

--- snap.cpp
+++ snap.cpp
@@ -7,11 +7,11 @@
 #include <chrono>
 #include <map>
 #include <stack>
+#include <tuple>
 
 using namespace std;
-using namespace std::chrono;
-typedef TPt<TNEANet> PNGraph;
-typedef pair<int, int> customPair;
+using namespace chrono;
+PNEANet g = TNEANet::New();
 
 struct oper{
     bool operator()(const std::tuple<int,int,int>& one, const std::tuple<int,int,int>& two)
@@ -21,27 +21,27 @@ struct oper{
 };
 
 
-void insertVertex(int value, PNGraph  &g) {
-    g.AddNode(value);
+void insertVertex(int value) {
+    g->AddNode(value);
 }
 
-void insertEdge(int a, int b, int weight, PNGraph &g) {
-    g.AddEdge(a,b);
+void insertEdge(int a, int b, int weight) {
+    g->AddEdge(a,b);
     TNEANet::TEdgeI EI = g->GetEI(a,b);
-    g.AddIntAttrDatE(EI, weight, "weight");
+    g->AddIntAttrDatE(EI, weight, "weight");
 }
 
-void deleteVertex(int value, PNGraph &g) {
+void deleteVertex(int value) {
     g->DelNode(value);
 }
 
-void deleteEdge(int a, int b, PNGraph &g) {
+void deleteEdge(int a, int b) {
     g->DelEdge(a,b);
 }
 
 
 
-void dfs(PNGraph * g, int startingNode) {
+void dfs(int startingNode) {
     stack<int> visiting;
     map<int, bool> travelled;
     visiting.push(startingNode);
@@ -61,14 +61,14 @@ void dfs(PNGraph * g, int startingNode) {
     }
 }
 
-void bfs(PNGraph * g, int startingNode){
+void bfs(int startingNode){
     std::queue<int> visiting;
     map<int,bool> travelled;
     visiting.push(startingNode);
     travelled[startingNode] = true;
     int checking, target;
     while (!visiting.empty()) {
-        checking = visiting.top();
+        checking = visiting.front();
         visiting.pop();
         TNEANet::TNodeI NI = g->GetNI(checking);
         for (int i = 0; i < NI.GetOutDeg(); i++) {
@@ -82,19 +82,18 @@ void bfs(PNGraph * g, int startingNode){
 
 }
 
-vector<pair<int, int>> prim(PNGraph * g, int vertexStart){
+vector<pair<int, int>> prim(int vertexStart){
     vector<pair<int, int>> result;
     priority_queue<std::tuple<int,int,int>,vector<std::tuple<int,int,int>>, oper> next;
     map<int,bool> travelled;
     std::tuple<int,int,int> now;
-    int weight;
-    pair<v_it,v_it> iterators = vertices(g);
     int checking, target, weight;
-    for(int i = 0; i < g->GetNodes; ++i){
+    for(int i = 0; i < g->GetNodes(); ++i){
         result.push_back(make_pair(-1, INT32_MAX));
     }
     result[checking].second = 0;
     next.push(make_tuple(0,checking,checking));
+    cout << "passed" << "\n";
     while(!next.empty()){
         now = next.top();
         next.pop();
@@ -115,15 +114,13 @@ vector<pair<int, int>> prim(PNGraph * g, int vertexStart){
     return result;
 }
 
-vector<pair<int, int>> dijkstra(PNGraph * g, int vertexStart){
+vector<pair<int, int>> dijkstra(int vertexStart){
     vector<pair<int, int>> result;
     priority_queue<std::tuple<int,int,int>,vector<std::tuple<int,int,int>>, oper> next;
     map<int,bool> travelled;
     std::tuple<int,int,int> now;
-    int weight;
-    pair<v_it,v_it> iterators = vertices(g);
     int checking, target, weight;
-    for(int i = 0; i < g->GetNodes; ++i){
+    for(int i = 0; i < g->GetNodes(); ++i){
         result.push_back(make_pair(INT32_MAX, -1));
     }
     result[checking].first = 0;
@@ -150,11 +147,10 @@ vector<pair<int, int>> dijkstra(PNGraph * g, int vertexStart){
 
 
 
-vector<vector<int>> floydWarshall(PNGraph * g){
+vector<vector<int>> floydWarshall(){
     vector<vector<int>> distances;
     unsigned long vNum = g->GetNodes();
     distances.resize(vNum);
-    pair<v_it,v_it> iterators = vertices(g);
     for(int it = 0; it  < g->GetNodes(); ++it){
         for(int it2 = 0; it2 < g->GetNodes(); ++it2){
             if(it == it2){
@@ -179,7 +175,7 @@ vector<vector<int>> floydWarshall(PNGraph * g){
 
 }
 
-vector<std::tuple<int, int, int>> kruskal (PNGraph * g) {
+vector<std::tuple<int, int, int>> kruskal () {
     priority_queue<std::tuple<int,int,int>,vector<std::tuple<int,int,int>>, oper> next;
     int vertex = g->GetNodes();
     vector<std::tuple<int, int, int>> result;
@@ -189,13 +185,7 @@ vector<std::tuple<int, int, int>> kruskal (PNGraph * g) {
     for (TNEANet::TEdgeI EI = g->BegEI(); EI < g->EndEI(); EI++) {
         origin = EI.GetSrcNId();
         target = EI.GetDstNId();
-        next.push(g->GetIntAttrDatE(EI, "weight"),origin,target);
-        int set_u = ds.find(origin);
-        int set_v = ds.find(target);
-        if (set_u != set_v){
-            cout << u << " - " << v << endl;
-            ds.merge(set_u, set_v, );
-        }
+        next.push(std::make_tuple((int)g->GetIntAttrDatE(EI, "weight"),(int)origin,(int)target));
     }
     while(!next.empty()){
         now = next.top();
@@ -213,50 +203,51 @@ vector<std::tuple<int, int, int>> kruskal (PNGraph * g) {
 }
 
 int main(){ //Example Program
+    for(int i = 1; i < 15; ++i) insertVertex(i);
 
-    PNGraph g = TNEANet::New();
+
     //Here we are inserting the edges of the graph provided
-    insertEdge(1, 3, 8, &g);
-    insertEdge(1, 4, 8, &g);
-    insertEdge(3, 2, 7, &g);
-    insertEdge(3, 10, 4,&g);
-    insertEdge(3, 5, 8, &g);
-    insertEdge(2, 5, 7, &g);
-    insertEdge(4, 8, 2, &g);
-    insertEdge(4, 7, 3, &g);
-    insertEdge(4, 5, 1, &g);
-    insertEdge(5, 6, 9, &g);
-    insertEdge(6, 13, 4, &g);
-    insertEdge(7, 4, 6, &g);
-    insertEdge(8, 7, 3, &g);
-    insertEdge(8, 9, 3, &g);
-    insertEdge(9, 10, 2, &g);
-    insertEdge(9, 12, 4, &g);
-    insertEdge(10, 3, 10, &g);
-    insertEdge(10, 6, 6, &g);
-    insertEdge(11, 12, 6, &g);
-    insertEdge(12, 9, 2, &g);
-    insertEdge(12, 11, 8, &g);
-    insertEdge(12, 14, 9, &g);
-    insertEdge(13, 14, 6, &g);
-    insertEdge(14, 13, 12, &g);
+    insertEdge(1, 3, 8);
+    insertEdge(1, 4, 8);
+    insertEdge(3, 2, 7);
+    insertEdge(3, 10, 4);
+    insertEdge(3, 5, 8);
+    insertEdge(2, 5, 7);
+    insertEdge(4, 8, 2);
+    insertEdge(4, 7, 3);
+    insertEdge(4, 5, 1);
+    insertEdge(5, 6, 9);
+    insertEdge(6, 13, 4);
+    insertEdge(7, 4, 6);
+    insertEdge(8, 7, 3);
+    insertEdge(8, 9, 3);
+    insertEdge(9, 10, 2);
+    insertEdge(9, 12, 4);
+    insertEdge(10, 3, 10);
+    insertEdge(10, 6, 6);
+    insertEdge(11, 12, 6);
+    insertEdge(12, 9, 2);
+    insertEdge(12, 11, 8);
+    insertEdge(12, 14, 9);
+    insertEdge(13, 14, 6);
+    insertEdge(14, 13, 12);
     /*
     insertVertex(g); //Here we insert a vertex
     insertEdge(1,5,3,g); //Here we insert an edge from vertex 1 to 5 with weight 3
     deleteVertex(15,g); //Here we delete the vertex 15
     deleteEdge(1,5,g); //Here we delete the edge going from Vertex 1 to 5.
     */
-    dfs(&g, 1); //Here we do a depth-first search, that will print the path
+    //dfs(1); //Here we do a depth-first search, that will print the path
 
-    bfs(&g, 1); //Here we do a breath-first search, that will print the path
+    //bfs(1); //Here we do a breath-first search, that will print the path
 
-    vector<pair<int, int>> Prim = prim(&g, 1); //Here we get a vector of pairs, telling us the minimum spanning tree according to prim
+    vector<pair<int, int>> Prim = prim(1); //Here we get a vector of pairs, telling us the minimum spanning tree according to prim
+    cout << "1" << "\n";
+    vector<std::tuple<int, int, int>> Kruskal = kruskal(); //Here we get a tuple of vertex and int, telling us the minimum spanning tree according to kruskal
 
-    vector<std::tuple<int, int, int>> Kruskal = kruskal(&g); //Here we get a tuple of vertex and int, telling us the minimum spanning tree according to kruskal
+    vector<pair<int, int>> Dijkstra = dijkstra(1); //Here we get the minimum distances from certain node to other ones using Dijkstra's algorithm
 
-    vector<pair<int, int>> Dijkstra = dijkstra(&g,1); //Here we get the minimum distances from certain node to other ones using Dijkstra's algorithm
-
-    vector<vector<int>> FloydWarhsall = floydWarshall(&g); //Here we get the minimum distances from all nodes to other ones using Floyd-Warshall's algorithm
+    vector<vector<int>> FloydWarhsall = floydWarshall(); //Here we get the minimum distances from all nodes to other ones using Floyd-Warshall's algorithm
 
     return 0;
 }
